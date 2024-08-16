70bbeeeb1a747020f49f9e07cc166f9fbdecc1aa
NoPixels4u <al_kurz@outlook.de>
2024-08-16 18:08:08 +0200

    gameplay changes

---
+++ Assets/Prefabs/CoalPile/CoalContainer.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using System.Collections;
+using UnityEngine;
+using Fusion;
+using Fusion.XRShared.Demo;
+using Unity.PlasticSCM.Editor.WebApi;
+
+public class CoalContainer : NetworkBehaviour
+{
+    private AddCoal addCoal;
+    private ResourceSpawnerPrefab resourceSpawnerPrefab;
+    private int currentHits;
+    [SerializeField] private int maxHitsToKill;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            if (maxHitsToKill == currentHits) {
+                AddCoal addCoal = GameObject.FindGameObjectWithTag(TagConstants.COAL_PILE).GetComponent<AddCoal>();
+                ResourceSpawnerPrefab resourceSpawnerPrefab = GameObject.FindGameObjectWithTag(TagConstants.WORLD_MANAGER).GetComponent<ResourceSpawnerPrefab>();
+                addCoal.spawnCoal();
+                resourceSpawnerPrefab.Despawn(gameObject);
+            } else {
+                currentHits++;
+            }
+        }
+    }
+}
