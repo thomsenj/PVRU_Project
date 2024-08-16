9534ef1e9b5bab4e9265783d6c866d56e1fd0b0d
NoPixels4u <al_kurz@outlook.de>
2024-08-16 12:45:10 +0200

    fix burn fuel

--- Assets/Prefabs/CoalPile/BurnCoal.cs
+++ Assets/Prefabs/CoalPile/BurnCoal.cs
@@ -4,106 +4,109 @@ using Fusion;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class BurnCoal : NetworkBehaviour
+namespace Fusion.XRShared.Demo
 {
-    public float fuelstand = 100f;
-
-    private TrainManager trainManager;
-    private float fuelModifier = 1.0f;
-    private GameOverManager gameOverManager;
-    [SerializeField] private float spawnInterval = 5.0f; 
-    private float spawnTimer;
-
-    private void Start()
+    public class BurnCoal : NetworkBehaviour
     {
-        try
+        public float fuelstand = 100f;
+
+        private TrainManager trainManager;
+        private float fuelModifier = 1.0f;
+        private GameOverManager gameOverManager;
+        [SerializeField] private float spawnInterval = 5.0f;
+        private float spawnTimer;
+
+        private void Start()
         {
-            trainManager = GameObject.FindGameObjectWithTag(TagConstants.WORLD_MANAGER).GetComponent<TrainManager>();
-            gameOverManager = GameObject.FindGameObjectWithTag(TagConstants.WORLD_MANAGER).GetComponent<GameOverManager>();
-            fuelModifier = trainManager.getFuelModifier();
-        }
-        catch
-        {
-            Debug.LogError("This scene lacks a train manager.");
-        }
-    }
-
-
-    public override void FixedUpdateNetwork()
-    {
-        base.FixedUpdateNetwork();
-
-        if (Object.HasStateAuthority)
-        {
-            spawnTimer += Time.fixedDeltaTime;
-
-            if (spawnTimer >= spawnInterval)
+            try
             {
-                fuelstand = Mathf.Clamp(fuelstand - (1 * fuelModifier), 0, 100);
-                //try { trainManager.setSpeed(getSpeed()); }
-                //catch { Debug.LogError("Could not set train speed."); }
-                if (fuelstand < 1)
+                trainManager = GameObject.FindGameObjectWithTag(TagConstants.WORLD_MANAGER).GetComponent<TrainManager>();
+                gameOverManager = GameObject.FindGameObjectWithTag(TagConstants.WORLD_MANAGER).GetComponent<GameOverManager>();
+                fuelModifier = trainManager.getFuelModifier();
+            }
+            catch
+            {
+                Debug.LogError("This scene lacks a train manager.");
+            }
+        }
+
+
+        public override void FixedUpdateNetwork()
+        {
+            base.FixedUpdateNetwork();
+
+            if (Object.HasStateAuthority)
+            {
+                spawnTimer += Time.fixedDeltaTime;
+
+                if (spawnTimer >= spawnInterval)
                 {
-                    gameOverManager.TriggerGameOver();
+                    fuelstand = fuelstand - (1 * fuelModifier);
+
+                    if (fuelstand < 1)
+                    {
+                        gameOverManager.TriggerGameOver();
+                    }
+                     spawnTimer = 0f;
                 }
             }
         }
-    }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag(TagConstants.COAL))
+        void OnTriggerEnter(Collider other)
         {
-            addFuel();
-            //if (trainManager != null)
-            //{
-            //    trainManager.setSpeed(getSpeed());
-            //} else
-            //{
-            //    Debug.LogError("This scene lacks a train manager.");
-            //}
-            NetworkObject networkObject = other.GetComponent<NetworkObject>();
-            if (networkObject != null)
+            if (other.CompareTag(TagConstants.COAL))
             {
-                Runner.Despawn(networkObject);
-            }
-            else
-            {
-                Debug.LogError("No NetworkObject component found on the colliding GameObject.");
+                addFuel();
+                //if (trainManager != null)
+                //{
+                //    trainManager.setSpeed(getSpeed());
+                //} else
+                //{
+                //    Debug.LogError("This scene lacks a train manager.");
+                //}
+                NetworkObject networkObject = other.GetComponent<NetworkObject>();
+                if (networkObject != null)
+                {
+                    Runner.Despawn(networkObject);
+                }
+                else
+                {
+                    Debug.LogError("No NetworkObject component found on the colliding GameObject.");
+                }
             }
         }
-    }
 
-    private void addFuel()
-    {
-        fuelstand = Mathf.Clamp(fuelstand + 10, 0, 100);
-    }
+        private void addFuel()
+        {
+            fuelstand = Mathf.Clamp(fuelstand + 10, 0, 100);
+        }
 
-    private float getSpeed()
-    {
+        private float getSpeed()
+        {
 
 
-        if (fuelstand >= 1 && fuelstand <= 10)
-            return 1;
-        else if (fuelstand > 10 && fuelstand <= 20)
-            return 2;
-        else if (fuelstand > 20 && fuelstand <= 30)
-            return 3;
-        else if (fuelstand > 30 && fuelstand <= 40)
-            return 4;
-        else if (fuelstand > 40 && fuelstand <= 50)
-            return 5;
-        else if (fuelstand > 50 && fuelstand <= 60)
-            return 6;
-        else if (fuelstand > 60 && fuelstand <= 70)
-            return 7;
-        else if (fuelstand > 70 && fuelstand <= 80)
-            return 8;
-        else if (fuelstand > 80 && fuelstand <= 90)
-            return 9;
-        else if (fuelstand > 90 && fuelstand <= 100)
-            return 10;
-        else
-            return 0;
+            if (fuelstand >= 1 && fuelstand <= 10)
+                return 1;
+            else if (fuelstand > 10 && fuelstand <= 20)
+                return 2;
+            else if (fuelstand > 20 && fuelstand <= 30)
+                return 3;
+            else if (fuelstand > 30 && fuelstand <= 40)
+                return 4;
+            else if (fuelstand > 40 && fuelstand <= 50)
+                return 5;
+            else if (fuelstand > 50 && fuelstand <= 60)
+                return 6;
+            else if (fuelstand > 60 && fuelstand <= 70)
+                return 7;
+            else if (fuelstand > 70 && fuelstand <= 80)
+                return 8;
+            else if (fuelstand > 80 && fuelstand <= 90)
+                return 9;
+            else if (fuelstand > 90 && fuelstand <= 100)
+                return 10;
+            else
+                return 0;
+        }
     }
 }
